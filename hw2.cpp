5edafb4465b99dcdfdcd1693992fdf28605254a9
彭永傑 <u10901149@o365.ttu.edu.tw>
2025-04-27 17:15:20 +0800

    測試，並且把第一題做了一半，剩下連續錯3次的部分還有點bug

---
+++ hw2.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+#include <time.h>
+
+int main(void)
+{
+	int password=0,n; 
+	
+	printf("這是E5A60的第二份程式作業\n");                                        //個人封面(題目1) 
+	printf("EEEEEE    55555        AAA          66666     00000\n");
+    printf("E        5            A   A        6          0   0\n");
+    printf("EEEEE     55555      AAAAAAA        66666     0   0\n");
+    printf("E              5    A       A      6     6    0   0\n");
+    printf("EEEEE     55555    A         A      66666     00000\n");
+	
+	system("pause");
+    system("cls");    
+for (n=1;n<=3;n++)
+    {    
+       printf("請輸入密碼(密碼為2025):");  
+       scanf("%d", &password);           //輸入密碼 
+
+        if(password==2025)               //密碼審核 
+           {
+		   printf("輸入正確，歡迎!!\n");
+		   system("pause");
+           system("cls");
+           break;
+		   }
+        else
+		   {
+			printf("輸入錯誤，請再輸入一次:");
+		   } 
+	} 
+
+
+
+}		   
